3242371fefa60663e8d988c2085f83645ca3e16e
Arjen <1663959583@qq.com>
2025-09-15 23:41:05 +0800

    Site updated: 2025-09-15 23:41:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/08/hello-world/","2025/09/15/Java-API/"];function toRandomPost(){
+var posts=["2025/09/15/Java-API/","2025/09/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
