e7560cee2f55e1f90f822945525128ee14c23cc9
Arjen <1663959583@qq.com>
2025-09-16 09:41:52 +0800

    Site updated: 2025-09-16 09:41:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/15/Java-API/","2025/09/08/hello-world/","2025/09/16/Java-Lambda/","2025/09/16/Java-Collection/"];function toRandomPost(){
+var posts=["2025/09/15/Java-API/","2025/09/16/Java-Lambda/","2025/09/08/hello-world/","2025/09/16/Java-Collection/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
