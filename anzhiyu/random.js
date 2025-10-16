1d8a5d53ac55a91624408fe9240abda52c72bac1
Arjen <1663959583@qq.com>
2025-10-16 09:43:08 +0800

    Site updated: 2025-10-16 09:43:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/17/Java-Exceptoin/","2025/09/15/Java-API/","2025/09/16/Java-Lambda/","2025/09/17/Java-File/","2025/09/17/Java-MethodRef/","2025/09/17/Java-Stream/","2025/09/17/Java-IO/","2025/09/16/Java-Collection/","2025/09/08/hello-world/","2025/09/18/Java-Thread/","2025/09/30/sz-bxsb/","2025/10/16/Java-webSocket/","2025/10/16/Java-Thread2/"];function toRandomPost(){
+var posts=["2025/09/16/Java-Collection/","2025/09/15/Java-API/","2025/09/17/Java-Exceptoin/","2025/09/17/Java-File/","2025/09/16/Java-Lambda/","2025/09/17/Java-IO/","2025/09/17/Java-MethodRef/","2025/09/17/Java-Stream/","2025/09/18/Java-Thread/","2025/10/16/Java-webSocket/","2025/09/08/hello-world/","2025/09/30/sz-bxsb/","2025/10/16/Java-Thread2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
